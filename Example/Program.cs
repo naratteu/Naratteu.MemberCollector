4ced2431bae3871011b7b9d6b2c9ecf8c8b8b34b
박성원 <skfhddlg@naver.com>
2024-06-24 15:19:36 +0900

    예제추가

---
+++ Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using Naratteu.MemberCollector;
+
+Console.WriteLine(string.Join(',',new Example.Program.Inner().Members));
+Console.WriteLine(string.Join(',',new Example.Program { B = "BB" }.Members));
+
+namespace Example
+{
+    partial class Program
+    {
+        [MemberCollect] public string A = "A";
+        [MemberCollect] public string B { get; set; } = "B";
+        [MemberCollect] public string C => "C";
+
+        public IEnumerable<object> Members => GetMemberCollection().Cast<object>();
+    
+        public partial class Inner
+        {
+            [MemberCollect] string A = "a";
+            [MemberCollect] string B { get; set; } = "b";
+            [MemberCollect] string C => "c";
+        
+            public IEnumerable<object> Members => GetMemberCollection().Cast<object>();
+        }
+    }
+}
